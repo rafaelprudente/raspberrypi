7ab382388a098331480ece8774175c9c8481d62f
Rafael Prudente dos Santos <rafael.prudente.santos@gmail.com>
2019-12-14 20:34:47 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,24 +4,29 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-clear
+#clear
 
 echo
 echo ---------- FIRST UPDATE/UPGRADE ----------
 echo 
-sudo DEBIAN_FRONTEND=noninteractive apt -yq update
-sudo DEBIAN_FRONTEND=noninteractive apt -yq full-upgrade
+#sudo DEBIAN_FRONTEND=noninteractive apt -yq update
+#sudo DEBIAN_FRONTEND=noninteractive apt -yq full-upgrade
+
+echo
+echo ---------- GATEWAY ----------
+gateway=route -n | grep UG | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+echo $gateway
 
 echo
 echo ---------- GIT ----------
 echo 
-sudo DEBIAN_FRONTEND=noninteractive apt -yq install git
+#sudo DEBIAN_FRONTEND=noninteractive apt -yq install git
 
 echo
 echo ---------- SSH ----------
 echo 
-sudo systemctl enable ssh
-sudo systemctl start ssh
+#sudo systemctl enable ssh
+#sudo systemctl start ssh
 
 echo
-echo -e "${GREEN}DONE!${NC}"
+echo -e "${GREEN}Done!${NC}"
