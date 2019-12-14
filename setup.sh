80f78c131bd613798a0111fd2085faa73b1d8c73
Rafael Prudente <32109908+rafaelprudente@users.noreply.github.com>
2019-12-14 18:37:18 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,20 +6,20 @@ NC='\033[0m'
 
 clear
 
-echo.
+echo
 echo ---------- FIRST UPDATE/UPGRADE ----------
-echo. 
+echo 
 sudo DEBIAN_FRONTEND=noninteractive apt -yq update
 sudo DEBIAN_FRONTEND=noninteractive apt -yq full-upgrade
 
-echo.
+echo
 echo ---------- GIT ----------
-echo. 
+echo 
 sudo DEBIAN_FRONTEND=noninteractive apt -yq install git
 
-echo.
+echo
 echo ---------- SSH ----------
-echo. 
+echo 
 sudo systemctl enable ssh
 sudo systemctl start ssh
 
