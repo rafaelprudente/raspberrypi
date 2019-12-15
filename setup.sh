91077310a20aa7d3240a7ef57f2b94b661274789
root <root@DESKTOP-BG0DVVA.localdomain>
2019-12-15 20:51:33 -0300

    setup raspberry

--- setup.sh
+++ setup.sh
@@ -2,17 +2,18 @@
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+CYAN='\033[0;36m'
 NC='\033[0m'
 
 clear
 
-echo ---------- FIRST UPDATE/UPGRADE ----------
+echo "${CYAN}---------- FIRST UPDATE/UPGRADE ----------${NC}"
 echo 
 sudo DEBIAN_FRONTEND=noninteractive apt -yq update
 sudo DEBIAN_FRONTEND=noninteractive apt -yq full-upgrade
 
 echo
-echo ---------- GATEWAY ----------
+echo "${CYAN}---------- GATEWAY ----------${NC}"
 gateway=$(route -n | grep UG | grep -E -o "(1[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)")
 echo "Gateway: $gateway"
 
@@ -27,31 +28,31 @@ fi
 echo "Fixed IP: $newIP"
 
 echo
-echo "---------- FIXED IP ($newIP) ----------"
-sudo cp /etc/dhcpcd.conf /etc/dhcpcd.conf.bkp
-echo "hostname" > /etc/dhcpcd.conf
-echo "clientid" >> /etc/dhcpcd.conf
-echo "persistent" >> /etc/dhcpcd.conf
-echo "option rapid_commit" >> /etc/dhcpcd.conf
-echo "option domain_name_servers, domain_name, domain_search, host_name" >> /etc/dhcpcd.conf
-echo "option classless_static_routes" >> /etc/dhcpcd.conf
-echo "option interface_mtu" >> /etc/dhcpcd.conf
-echo "require dhcp_server_identifier" >> /etc/dhcpcd.conf
-echo "slaac private" >> /etc/dhcpcd.conf
-echo "static ip_address=$newIP/24" >> /etc/dhcpcd.conf
-echo "static routers=$gateway" >> /etc/dhcpcd.conf
-echo "static domain_name_servers=$gateway" >> /etc/dhcpcd.conf
+echo "${CYAN}---------- FIXED IP ($newIP) ----------${NC}"
+#sudo cp /etc/dhcpcd.conf /etc/dhcpcd.conf.bkp
+#echo "hostname" > /etc/dhcpcd.conf
+#echo "clientid" >> /etc/dhcpcd.conf
+#echo "persistent" >> /etc/dhcpcd.conf
+#echo "option rapid_commit" >> /etc/dhcpcd.conf
+#echo "option domain_name_servers, domain_name, domain_search, host_name" >> /etc/dhcpcd.conf
+#echo "option classless_static_routes" >> /etc/dhcpcd.conf
+#echo "option interface_mtu" >> /etc/dhcpcd.conf
+#echo "require dhcp_server_identifier" >> /etc/dhcpcd.conf
+#echo "slaac private" >> /etc/dhcpcd.conf
+#echo "static ip_address=$newIP/24" >> /etc/dhcpcd.conf
+#echo "static routers=$gateway" >> /etc/dhcpcd.conf
+#echo "static domain_name_servers=$gateway" >> /etc/dhcpcd.conf
 
 #sudo cat /etc/dhcpcd.conf
 #sudo cp /etc/dhcpcd.conf.bkp /etc/dhcpcd.conf
 
 echo
-echo ---------- GIT ----------
+echo "${CYAN}---------- GIT ----------${NC}"
 echo 
 sudo DEBIAN_FRONTEND=noninteractive apt -yq install git
 
 echo
-echo ---------- SSH ----------
+echo "${CYAN}---------- SSH ----------${NC}"
 echo 
 sudo systemctl enable ssh
 sudo systemctl start ssh
