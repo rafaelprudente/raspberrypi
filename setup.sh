e62c3f43bb269d8c68be2a1535b857c84dd75c58
Rafael Prudente dos Santos <rafael.prudente.santos@gmail.com>
2019-12-15 00:31:16 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,6 +16,22 @@ echo
 echo ---------- GATEWAY ----------
 gateway=route -n | grep UG | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
 echo $gateway
+sudo cp /etc/dhcpcd.conf /etc/dhcpcd.conf.bkp
+echo "hostname\n" > /etc/dhcpcd.conf
+echo "clientid\n" >> /etc/dhcpcd.conf
+echo "persistent\n" >> /etc/dhcpcd.conf
+echo "option rapid_commit\n" >> /etc/dhcpcd.conf
+echo "option domain_name_servers, domain_name, domain_search, host_name\n" >> /etc/dhcpcd.conf
+echo "option classless_static_routes\n" >> /etc/dhcpcd.conf
+echo "option interface_mtu\n" >> /etc/dhcpcd.conf
+echo "require dhcp_server_identifier\n" >> /etc/dhcpcd.conf
+echo "slaac private\n" >> /etc/dhcpcd.conf
+echo "static ip_address=192.168.1.200/24\n" >> /etc/dhcpcd.conf
+echo "static routers=$gateway\n" >> /etc/dhcpcd.conf
+echo "static domain_name_servers=$gateway\n" >> /etc/dhcpcd.conf
+
+sudo cat /etc/dhcpcd.conf
+sudo cp /etc/dhcpcd.conf.bkp /etc/dhcpcd.conf
 
 echo
 echo ---------- GIT ----------
