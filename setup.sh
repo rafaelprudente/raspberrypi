b2fb1e58ac5c3784f8b7bfa6eb0356b3346f85b6
rafaelprudente <rafael.prudente.santos@gmail.com>
2019-12-15 23:03:16 -0300

    setup raspberry

--- setup.sh
+++ setup.sh
@@ -13,19 +13,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt -yq update
 sudo DEBIAN_FRONTEND=noninteractive apt -yq full-upgrade
 
 echo
-echo -e "${CYAN}---------- GIT ----------${NC}"
-echo 
-sudo DEBIAN_FRONTEND=noninteractive apt -yq install git
-
-git config --global user.name "rafaelprudente"
-git config --global user.email rafael.prudente.santos@gmail.com
-
-echo
-echo -e "${CYAN}---------- ARP-SCAN ----------${NC}"
-echo 
-sudo DEBIAN_FRONTEND=noninteractive apt -yq install arp-scan
-
-echo
 echo -e "${CYAN}---------- GATEWAY ----------${NC}"
 gateway=$(route -n | grep UG | grep -E -o "(1[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)")
 echo "Gateway: $gateway"
@@ -33,52 +20,77 @@ echo "Gateway: $gateway"
 IFS='.'
 read -ra ipAddr <<< "$gateway"
 IFS=' '
-if [ "$#" -ne 1 ] ; then
-  newIP="${ipAddr[0]}.${ipAddr[1]}.${ipAddr[2]}.200"
-else  
-  newIP="${ipAddr[0]}.${ipAddr[1]}.${ipAddr[2]}.$1"
-fi
-echo "Fixed IP: $newIP"
+rootIP="${ipAddr[0]}.${ipAddr[1]}.${ipAddr[2]}"
 
 echo
-echo -e "${CYAN}---------- FIXED IP ($newIP) ----------${NC}"
-#sudo cp /etc/dhcpcd.conf /etc/dhcpcd.conf.bkp
-#echo "hostname" > /etc/dhcpcd.conf
-#echo "clientid" >> /etc/dhcpcd.conf
-#echo "persistent" >> /etc/dhcpcd.conf
-#echo "option rapid_commit" >> /etc/dhcpcd.conf
-#echo "option domain_name_servers, domain_name, domain_search, host_name" >> /etc/dhcpcd.conf
-#echo "option classless_static_routes" >> /etc/dhcpcd.conf
-#echo "option interface_mtu" >> /etc/dhcpcd.conf
-#echo "require dhcp_server_identifier" >> /etc/dhcpcd.conf
-#echo "slaac private" >> /etc/dhcpcd.conf
-#echo "static ip_address=$newIP/24" >> /etc/dhcpcd.conf
-#echo "static routers=$gateway" >> /etc/dhcpcd.conf
-#echo "static domain_name_servers=$gateway" >> /etc/dhcpcd.conf
-
-#sudo cat /etc/dhcpcd.conf
-#sudo cp /etc/dhcpcd.conf.bkp /etc/dhcpcd.conf
-
-echo
-echo -e "${CYAN}---------- GIT ----------${NC}"
+echo -e "${CYAN}---------- ARP-SCAN ----------${NC}"
 echo 
-sudo DEBIAN_FRONTEND=noninteractive apt -yq install git
+sudo DEBIAN_FRONTEND=noninteractive apt -yq install arp-scan
 
-git config --global user.name "rafaelprudente"
-git config --global user.email rafael.prudente.santos@gmail.com
+usedIPS=$(arp-scan -I eth0 ${ipAddr[0]}.${ipAddr[1]}.${ipAddr[2]}.0/24 |  grep -E -o "(1[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))
+for i in "${usedIPS[@]}"; do
+    echo "$i"
+done
 
-echo
-echo -e "${CYAN}---------- SSH ----------${NC}"
-echo 
-sudo systemctl enable ssh
-sudo systemctl start ssh
 
-echo
-read -p "Reboot now [Y/N]? " -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    echo
-    echo -e "${GREEN}Done!${NC}"
-    exit 0
-fi
-sudo reboot
+
+
+
+
+#echo
+#echo -e "${CYAN}---------- GATEWAY ----------${NC}"
+#gateway=$(route -n | grep UG | grep -E -o "(1[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)")
+#echo "Gateway: $gateway"
+#
+#IFS='.'
+#read -ra ipAddr <<< "$gateway"
+#IFS=' '
+#if [ "$#" -ne 1 ] ; then
+#  newIP="${ipAddr[0]}.${ipAddr[1]}.${ipAddr[2]}.200"
+#else  
+#  newIP="${ipAddr[0]}.${ipAddr[1]}.${ipAddr[2]}.$1"
+#fi
+#echo "Fixed IP: $newIP"
+#
+#echo
+#echo -e "${CYAN}---------- FIXED IP ($newIP) ----------${NC}"
+##sudo cp /etc/dhcpcd.conf /etc/dhcpcd.conf.bkp
+##echo "hostname" > /etc/dhcpcd.conf
+##echo "clientid" >> /etc/dhcpcd.conf
+##echo "persistent" >> /etc/dhcpcd.conf
+##echo "option rapid_commit" >> /etc/dhcpcd.conf
+##echo "option domain_name_servers, domain_name, domain_search, host_name" >> /etc/dhcpcd.conf
+##echo "option classless_static_routes" >> /etc/dhcpcd.conf
+##echo "option interface_mtu" >> /etc/dhcpcd.conf
+##echo "require dhcp_server_identifier" >> /etc/dhcpcd.conf
+##echo "slaac private" >> /etc/dhcpcd.conf
+##echo "static ip_address=$newIP/24" >> /etc/dhcpcd.conf
+##echo "static routers=$gateway" >> /etc/dhcpcd.conf
+##echo "static domain_name_servers=$gateway" >> /etc/dhcpcd.conf
+#
+##sudo cat /etc/dhcpcd.conf
+##sudo cp /etc/dhcpcd.conf.bkp /etc/dhcpcd.conf
+#
+#echo
+#echo -e "${CYAN}---------- GIT ----------${NC}"
+#echo 
+#sudo DEBIAN_FRONTEND=noninteractive apt -yq install git
+#
+#git config --global user.name "rafaelprudente"
+#git config --global user.email rafael.prudente.santos@gmail.com
+#
+#echo
+#echo -e "${CYAN}---------- SSH ----------${NC}"
+#echo 
+#sudo systemctl enable ssh
+#sudo systemctl start ssh
+#
+#echo
+#read -p "Reboot now [Y/N]? " -n 1 -r
+#if [[ ! $REPLY =~ ^[Yy]$ ]]
+#then
+#    echo
+#    echo -e "${GREEN}Done!${NC}"
+#    exit 0
+#fi
+#sudo reboot
