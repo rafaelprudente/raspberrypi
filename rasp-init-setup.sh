61c41cb170061b1935b3739cdc38ccc6892ec2ae
rafaelprudente <rafael.prudente.santos@gmail.com>
2020-01-06 23:30:38 -0300

    20200106

--- rasp-init-setup.sh
+++ rasp-init-setup.sh
@@ -9,29 +9,29 @@ export DEBIAN_FRONTEND=noninteractive
 
 #echo 10 | whiptail --title "Initial Setup" --gauge "Perform Update" 8 78 0
 echo -e "${CYAN}---------- FIRST UPDATE ----------${NC}"
-sudo apt -yq update
+sudo apt -yq update >> rasp-init-setup.log
 
 #echo 20 | whiptail --title "Initial Setup" --gauge "Perform Upgrade" 8 78 0
 echo -e "${CYAN}---------- FIRST UPGRADE ----------${NC}"
-sudo apt -yq full-upgrade
+sudo apt -yq full-upgrade >> rasp-init-setup.log
 
 #echo 30 | whiptail --title "Initial Setup" --gauge "Perform WHIPTAIL Install" 8 78 0
 echo -e "${CYAN}---------- WHIPTAIL ----------${NC}"
-sudo apt -yq install whiptail
+sudo apt -yq install whiptail >> rasp-init-setup.log
 
 echo 40 | whiptail --title "Initial Setup" --gauge "Perform GIT Install" 8 78 0
 #echo -e "${CYAN}---------- GIT ----------${NC}"
-sudo apt -yq install git
+sudo apt -yq install git >> rasp-init-setup.log
 
 echo 40 | whiptail --title "Initial Setup" --gauge "Perform ARP-SCAN Install" 8 78 0
 #echo -e "${CYAN}---------- ARP-SCAN ----------${NC}"
-sudo apt -yq install arp-scan
+sudo apt -yq install arp-scan >> rasp-init-setup.log
 
 echo 50 | whiptail --title "Initial Setup" --gauge "Perform JAVA 8 Install" 8 78 0
 #echo -e "${CYAN}---------- INSTALL JAVA 8 ----------${NC}"
-sudo apt -yq install openjdk-8-jre
+sudo apt -yq install openjdk-8-jre >> rasp-init-setup.log
 
 echo 60 | whiptail --title "Initial Setup" --gauge "Perform JAVA 11 Install" 8 78 0
 #echo -e "${CYAN}---------- INSTALL JAVA 11 ----------${NC}"
-sudo apt -yq install openjdk-11-jre
+sudo apt -yq install openjdk-11-jre >> rasp-init-setup.log
 
